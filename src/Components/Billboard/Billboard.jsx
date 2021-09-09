a1e1525e346c3df6078627ea41b2690f3fa11f84
Skullomanic84 <jhshiloh@gmail.com>
2021-09-09 21:07:24 +0200

    updated billboard.js

--- src/Components/Billboard/Billboard.jsx
+++ src/Components/Billboard/Billboard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styles from './Billboard.module.css'
+import HeroImage from './Assets/billboard.webp'
+import HeroImageTitle from './Assets/billboard-title.webp'
 
 function Billboard() {
     return (
-        <div>
-            Billboard
+        <div className={styles.billboard}>
+            <div className={styles.innerbillboard}>
+            <img src={HeroImage} alt="Abstract: The art of design"/>
+            <div className={styles.info}>
+            <img src={HeroImageTitle} alt="" srcset="" /> 
+            </div>
+            </div>
         </div>
     )
 }
